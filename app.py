36e635595cd30026ecb47a40b896044d469d2552
Kaylee <Kamchale1@sheffielf.ac.uk>
2019-02-27 19:38:03 +0000

    Edits

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
-  return "hello world!"
+  return render_template("index.html")
 
 app.run(debug=True)
