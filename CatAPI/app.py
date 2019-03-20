c451fa62763fe77295f2664c45a33a7ada92d97e
Kaylee <Kamchale1@sheffielf.ac.uk>
2019-03-20 19:54:22 +0000

    Added .env file

--- CatAPI/app.py
+++ CatAPI/app.py
@@ -1,14 +1,18 @@
+import os
 import requests
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+from dotenv import load_dotenv
+load_dotenv()
+
 @app.route('/')
 def index():
     response = requests.get(
         'https://api.thecatapi.com/v1/images/search',
-        headers={'x-api-key': '05b0b1c7-5734-452c-b012-d2d4cc4945db'}
+        headers={'x-api-key': os.getenv('API_KEY')}
     )
     data = response.json()[0]
     print(data)
@@ -22,10 +26,10 @@ def add_favourite():
 
     response = requests.post(
         'https://api.thecatapi.com/v1/favourites',
-        headers={'x-api-key': '05b0b1c7-5734-452c-b012-d2d4cc4945db'},
+        headers={'x-api-key': os.getenv('API_KEY')},
         json={
             'image_id': image_id,
-            'sub_id': 'j14f94'
+            'sub_id': os.getenv('SUB_ID')
         }
     )
     data = response.json()
@@ -36,7 +40,7 @@ def add_favourite():
 def see_favourites():
     response = requests.get(
         'https://api.thecatapi.com/v1/favourites?limit=3',
-        headers={'x-api-key': '05b0b1c7-5734-452c-b012-d2d4cc4945db'}
+        headers={'x-api-key': os.getenv('API_KEY')}
     )
     data = response.json()
     print(data)
